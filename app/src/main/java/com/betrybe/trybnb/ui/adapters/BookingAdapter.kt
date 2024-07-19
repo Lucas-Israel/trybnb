d7ce754dfd15374a433a822298e52db1d7a9df1e
Lucas Israel <lisraelalves92@gmail.com>
2024-07-19 14:37:52 -0300

    removed databinding directly in the xml, added its logic to BookingAdapter

--- app/src/main/java/com/betrybe/trybnb/ui/adapters/BookingAdapter.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/adapters/BookingAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.betrybe.trybnb.R
 import com.betrybe.trybnb.data.models.Booking
 import com.betrybe.trybnb.databinding.ListItemReservationBinding
 
@@ -12,11 +13,19 @@ class BookingAdapter(private val bookings: List<Booking>) :
 
     private lateinit var binding: ListItemReservationBinding
 
-
     class BookingViewHolder(private val binding: ListItemReservationBinding) :
         ViewHolder(binding.root) {
         fun bind(bookings: Booking) {
-            binding.vm = bookings
+            binding.nameItemReservation.text = bookings.fullName
+            binding.checkinItemReservation.text = bookings.checkin
+            binding.checkoutItemReservation.text = bookings.checkout
+            binding.additionalNeedsItemReservation.text = bookings.aditionalNeeds
+            binding.totalPriceItemReservation.text = bookings.price
+            if (bookings.depositpaid) {
+                binding.depositpaidItemReservation.setImageResource(R.drawable.ic_depositpaid_true)
+            } else {
+                binding.depositpaidItemReservation.setImageResource(R.drawable.ic_depositpaid_false)
+            }
         }
     }
 
