a20624c730cf65b8771f7ea183231746d7ea733f
Lucas Israel <lisraelalves92@gmail.com>
2024-07-31 13:49:16 -0300

    refactor: changed to use safeApiCall

--- app/src/main/java/com/betrybe/trybnb/data/repository/BookingRepository.kt
+++ app/src/main/java/com/betrybe/trybnb/data/repository/BookingRepository.kt
@@ -6,45 +6,24 @@ import com.betrybe.trybnb.model.Booking
 import com.betrybe.trybnb.data.utils.ClientResult
 import com.betrybe.trybnb.data.network.BookingDataSource
 import com.betrybe.trybnb.data.utils.BookingCreatorHelper
-import com.betrybe.trybnb.data.utils.DispatcherProvider
 import com.betrybe.trybnb.data.response.Reservation
-import kotlinx.coroutines.withContext
+import com.betrybe.trybnb.data.utils.safeApiCall
 import javax.inject.Inject
 
 class BookingRepository @Inject constructor(private val bookingDS: BookingDataSource) {
 
     suspend fun getBookings(): ClientResult<List<Booking>> {
-        return withContext(DispatcherProvider.IO) {
-
-            val bookingsResponse =
-                bookingDS.getBookings()?.take(QUANTITY_OF_BOOKINGS_AT_A_TIME)
-                    ?: return@withContext ClientResult.ClientError(
-                        isServerError = false,
-                        isNetworkError = true
-                    )
-
-            val bookingList = mutableListOf<Booking>()
-
-            bookingsResponse.forEach {
+        return safeApiCall {
+            bookingDS.getBookings().take(QUANTITY_OF_BOOKINGS_AT_A_TIME).map {
                 val bookingById = bookingDS.getBookingById(it.bookingid.toString())
-                if (bookingById != null) {
-                    val booking = BookingCreatorHelper().bookingCreatorFromBook(bookingById)
-                    bookingList.add(booking)
-                }
+                BookingCreatorHelper().bookingCreatorFromReservation(bookingById)
             }
-            return@withContext ClientResult.ClientSuccess(bookingList)
         }
     }
 
     suspend fun createBooking(booking: Reservation): ClientResult<CreateBooking> {
-        return withContext(DispatcherProvider.IO) {
-            val bookingResponse = bookingDS.createBooking(booking)
-                ?: return@withContext ClientResult.ClientError(
-                    isServerError = false,
-                    isNetworkError = true
-                )
-            return@withContext ClientResult.ClientSuccess(bookingResponse)
+        return safeApiCall {
+            bookingDS.createBooking(booking)
         }
     }
 }
-
--- app/src/main/java/com/betrybe/trybnb/data/repository/LoginRepository.kt
+++ app/src/main/java/com/betrybe/trybnb/data/repository/LoginRepository.kt
@@ -3,25 +3,15 @@ package com.betrybe.trybnb.data.repository
 import com.betrybe.trybnb.data.utils.ClientResult
 import com.betrybe.trybnb.data.response.Token
 import com.betrybe.trybnb.data.network.LoginDataSource
-import com.betrybe.trybnb.data.utils.DispatcherProvider
-import kotlinx.coroutines.withContext
+import com.betrybe.trybnb.data.utils.safeApiCall
 import javax.inject.Inject
 
 class LoginRepository @Inject constructor(private val loginDS: LoginDataSource) {
 
-    suspend fun login(email: String, password: String): ClientResult<Token?> {
+    suspend fun login(email: String, password: String): ClientResult<Token> {
 
-        return withContext(DispatcherProvider.IO) {
-            val response = loginDS.login(email, password)
-
-            if (response?.token == null || response.token.isEmpty()) {
-                return@withContext ClientResult.ClientError(
-                    isServerError = false,
-                    isNetworkError = true
-                )
-            }
-            
-            return@withContext ClientResult.ClientSuccess(response)
+        return safeApiCall {
+            loginDS.login(email, password)
         }
     }
 }
