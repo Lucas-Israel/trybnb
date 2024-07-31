4762d27e2a234c6c16a7e25ae4d22f9a2351c056
Lucas Israel <lisraelalves92@gmail.com>
2024-07-31 13:42:51 -0300

    removed Response abstraction from ApiService.kt

--- app/src/main/java/com/betrybe/trybnb/data/api/ApiService.kt
+++ app/src/main/java/com/betrybe/trybnb/data/api/ApiService.kt
@@ -6,7 +6,6 @@ import com.betrybe.trybnb.data.response.CreateBooking
 import com.betrybe.trybnb.model.LoginBody
 import com.betrybe.trybnb.data.response.Reservation
 import com.betrybe.trybnb.data.response.Token
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -16,16 +15,16 @@ import retrofit2.http.Path
 interface ApiService {
 
     @POST("auth")
-    suspend fun login(@Body auth: LoginBody): Response<Token>
+    suspend fun login(@Body auth: LoginBody): Token
 
     @GET("booking")
-    suspend fun getBookings(): Response<List<BookingId>>
+    suspend fun getBookings(): List<BookingId>
 
     @GET("booking/{id}")
     @Headers("Accept: application/json")
-    suspend fun getBookingById(@Path("id") id: String): Response<Reservation>
+    suspend fun getBookingById(@Path("id") id: String): Reservation
 
     @POST("booking")
     @Headers("Accept: application/json")
-    suspend fun createBooking(@Body body: Reservation): Response<CreateBooking>
+    suspend fun createBooking(@Body body: Reservation): CreateBooking
 }
