54d3b0fb46bf0968294af12558a15e41562de9bf
Lucas Israel <lisraelalves92@gmail.com>
2024-08-01 13:16:11 -0300

    extracted safeApiCall to its own file

--- app/src/main/java/com/betrybe/trybnb/common/utils/ClientResult.kt
+++ app/src/main/java/com/betrybe/trybnb/common/utils/ClientResult.kt
@@ -1,12 +1,5 @@
 package com.betrybe.trybnb.common.utils
 
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import retrofit2.HttpException
-import java.io.IOException
-
 sealed class ClientResult<out T> {
 
     /**
@@ -22,47 +15,4 @@ sealed class ClientResult<out T> {
      */
     data class ClientError(val isServerError: Boolean, val isNetworkError: Boolean) :
         ClientResult<Nothing>()
-}
-
-suspend fun <T> safeApiCall(
-    dispatcher: CoroutineDispatcher = Dispatchers.IO,
-    apiCall: suspend () -> T
-): ClientResult<T> {
-    return withContext(dispatcher) {
-        try {
-            val response = apiCall()
-            ClientResult.ClientSuccess(response)
-        } catch (e: Exception) {
-            when (e) {
-                is CancellationException -> {
-                    // Rethrow cancellation exception to ensure coroutines are cancelled correctly
-                    throw e
-                }
-
-                is HttpException -> {
-                    // Handle HTTP errors, assuming it's a server error
-                    ClientResult.ClientError(
-                        isNetworkError = true,
-                        isServerError = false
-                    )
-                }
-
-                is IOException -> {
-                    // Handle network errors (e.g., no connectivity)
-                    ClientResult.ClientError(
-                        isNetworkError = false,
-                        isServerError = true
-                    )
-                }
-
-                else -> {
-                    // Handle any other unexpected exceptions
-                    ClientResult.ClientError(
-                        isNetworkError = true,
-                        isServerError = false
-                    )
-                }
-            }
-        }
-    }
 }
