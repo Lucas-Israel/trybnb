efc396bed76a1234d647ff6c31a5dfe7f89ef651
Lucas Israel <lisraelalves92@gmail.com>
2024-08-05 15:12:27 -0300

    refactored fragments for readability

--- app/src/main/java/com/betrybe/trybnb/ui/views/fragments/CreateReservationFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/CreateReservationFragment.kt
@@ -8,21 +8,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.betrybe.trybnb.R
-import com.betrybe.trybnb.model.BookingDates
+import com.betrybe.trybnb.common.utils.BookingCreatorHelper.reservationBodyCreator
+import com.betrybe.trybnb.common.utils.TextInputLayoutUtils.containerInputsValidationFlow
 import com.betrybe.trybnb.databinding.FragmentCreateReservationBinding
-import com.betrybe.trybnb.data.response.Reservation
 import com.betrybe.trybnb.ui.viewmodels.BookingViewModel
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputLayout
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 @AndroidEntryPoint
 class CreateReservationFragment : Fragment() {
-    private lateinit var binding: FragmentCreateReservationBinding
-    private val viewModel: BookingViewModel by viewModels()
+
+    private val bookingVm: BookingViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,86 +27,41 @@ class CreateReservationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_create_reservation, container, false)
-        binding = FragmentCreateReservationBinding.bind(view)
+        val binding = FragmentCreateReservationBinding.bind(view)
 
         binding.createReservationButton.setOnClickListener {
-            var readyToPost = true
-            val reservationLinearLayout: ViewGroup =
-                view.findViewById(R.id.reservation_linear_layout)
-            // Looping through all child views in the LinearLayout
-            for (i in 0 until reservationLinearLayout.childCount) {
-                val child = reservationLinearLayout.getChildAt(i)
-                // Only TextInputLayout children go through
-                if (child !is TextInputLayout) continue
-                isInputEmpty(child)
-                if (child.error != null) {
-                    readyToPost = false
-                }
-            }
+            val areInputsValid = containerInputsValidationFlow(container!!)
 
-            if (readyToPost) {
-                val checkin =
-                    reformatDate(binding.checkinCreateReservation.editText?.text.toString())
-                val checkout =
-                    reformatDate(binding.checkoutCreateReservation.editText?.text.toString())
-                lateinit var bookingDates: BookingDates
-                if (checkin != null && checkout != null) {
-                    bookingDates = BookingDates(checkin, checkout)
+            when (areInputsValid) {
+                true -> {
+                    val body = reservationBodyCreator(binding)
 
-                    val body = Reservation(
-                        firstname = binding.firstNameCreateReservation.editText?.text.toString(),
-                        lastname = binding.lastNameCreateReservation.editText?.text.toString(),
-                        totalprice = binding.totalPriceCreateReservation.editText?.text.toString()
-                            .toInt(),
-                        depositpaid = binding.depositpaidCreateReservation.isChecked,
-                        bookingdates = bookingDates,
-                        additionalneeds = binding.additionalNeedsCreateReservation.editText?.text.toString()
-                    )
-                    viewModel.createBooking(body)
+                    bookingVm.createBooking(body)
+
+                    reservationCreationSuccessMessage(binding)
                 }
-                viewLifecycleOwner.lifecycleScope.launch {
-                    viewModel.isBookingCreationSuccess.collect { success ->
-                        if (success) Snackbar.make(
-                            binding.createReservationScrollView,
-                            getString(R.string.reservation_success),
-                            Snackbar.LENGTH_SHORT
-                        ).show()
-                    }
-                }
+
+                false -> return@setOnClickListener
             }
         }
 
         return binding.root
     }
 
-    private fun isInputEmpty(input: TextInputLayout) {
-        if (input.editText?.text?.isEmpty() == true) {
-            input.error =
-                "O campo ${
-                    input.hint.toString()
-                        .replace(" do hóspede", "")
-                        .replace("adicionais", "Adicionais")
-                        .replace("total", "Total")
-                } é obrigatório"
-//            os testes da trybe não permitem fazer uma string interpolation esses replaces servem simplesmente para passar no avaliador
-        } else {
-            input.error = null
-        }
-    }
-
-    private fun reformatDate(inputDate: String): String? {
-        val inputFormatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-        val outputFormatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-
-        return try {
-            val date = inputFormatter.parse(inputDate)
-            if (date != null) {
-                outputFormatter.format(date)
-            } else {
-                null
+    /**
+     * Shows a Snackbar with a success message when a reservation is successfully created.
+     */
+    private fun reservationCreationSuccessMessage(bind: FragmentCreateReservationBinding) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            bookingVm.isBookingCreationSuccess.collect { success ->
+                success?.let {
+                    Snackbar.make(
+                        bind.createReservationScrollView,
+                        getString(R.string.reservation_success),
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
             }
-        } catch (e: Exception) {
-            null
         }
     }
 
--- app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ProfileFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ProfileFragment.kt
@@ -1,6 +1,5 @@
 package com.betrybe.trybnb.ui.views.fragments
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,8 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.betrybe.trybnb.R
-import com.betrybe.trybnb.UiText
-import com.betrybe.trybnb.common.utils.TextInputLayoutUtils.validateEmptyInput
+import com.betrybe.trybnb.common.utils.TextInputLayoutUtils.containerInputsValidationFlow
 import com.betrybe.trybnb.databinding.FragmentProfileBinding
 import com.betrybe.trybnb.ui.viewmodels.ProfileViewModel
 import com.google.android.material.snackbar.Snackbar
@@ -31,41 +29,39 @@ class ProfileFragment : Fragment() {
         val binding = FragmentProfileBinding.bind(view)
 
         binding.loginButtonProfile.setOnClickListener {
-            val emailInput = binding.loginInputProfile
-            val passInput = binding.passwordInputProfile
-            val email = emailInput.editText?.text.toString()
-            val password = passInput.editText?.text.toString()
+            val areInputsValid = containerInputsValidationFlow(container!!)
 
-            if (context != null) {
-                val emailEmptyWarning =
-                    UiText.StringResource(R.string.email_empty_warning).asString(requireContext())
+            when (areInputsValid) {
+                true -> {
+                    profileVM.login(
+                        email = binding.loginInputProfile.editText?.text.toString(),
+                        password = binding.passwordInputProfile.editText?.text.toString()
+                    )
 
-                val passwordEmptyWarning = UiText.StringResource(R.string.password_empty_warning)
-                    .asString(requireContext())
-
-                validateEmptyInput(emailInput, emailEmptyWarning)
-                validateEmptyInput(passInput, passwordEmptyWarning)
-            }
-
-            if (email.isEmpty() || password.isEmpty()) return@setOnClickListener
-
-            profileVM.login(email, password)
-
-            viewLifecycleOwner.lifecycleScope.launch {
-                profileVM.loginFailure.collect { error ->
-                    if (!error) successLoginMessage(binding)
+                    successLoginMessage(binding)
                 }
+
+                false -> return@setOnClickListener
             }
         }
         return view
     }
 
+    /**
+     * Shows a Snackbar with a success message when a login is successful.
+     */
     private fun successLoginMessage(binding: FragmentProfileBinding) {
-        Snackbar.make(
-            binding.profileScrollView,
-            getString(R.string.login_success),
-            Snackbar.LENGTH_SHORT
-        ).show()
+        viewLifecycleOwner.lifecycleScope.launch {
+            profileVM.token.collect { token ->
+                token?.let {
+                    Snackbar.make(
+                        binding.profileScrollView,
+                        getString(R.string.login_success),
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 
 }
