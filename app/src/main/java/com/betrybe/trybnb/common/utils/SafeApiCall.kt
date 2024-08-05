1bd3c2c6d98de6a591b81849f755d7a3e967b4df
Lucas Israel <lisraelalves92@gmail.com>
2024-08-05 14:52:55 -0300

    renamed from safeApiCall to SafeApiCall

--- app/src/main/java/com/betrybe/trybnb/common/utils/SafeApiCall.kt
+++ app/src/main/java/com/betrybe/trybnb/common/utils/SafeApiCall.kt
@@ -7,6 +7,13 @@ import retrofit2.HttpException
 import java.io.IOException
 import kotlin.coroutines.cancellation.CancellationException
 
+/**
+ * A helper function to make safe API calls.
+ * @param dispatcher The coroutine dispatcher to use for making the API call.
+ * @param apiCall The suspend function representing the API call.
+ * @return A [ClientResult] indicating the success or failure of the API call.
+ * @throws CancellationException if the API call is cancelled.
+ */
 suspend fun <T> safeApiCall(
     dispatcher: CoroutineDispatcher = Dispatchers.IO,
     apiCall: suspend () -> T
